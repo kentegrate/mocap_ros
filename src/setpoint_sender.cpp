4b6a5da4f885986ff6d7ae1d444969bec90af894
Ken Takaki <kenkiki94305@gmail.com>
2017-06-29 22:15:19 +0900

    changed to official setpoint sender

--- src/setpoint_sender.cpp
+++ src/setpoint_sender.cpp
@@ -1,47 +1,87 @@
+/**
+ * @file offb_node.cpp
+* @brief offboard example node, written with mavros version 0.14.2, px4 flight
+   * stack and tested in Gazebo SITL
+   */
+
 #include <ros/ros.h>
-#include <std_msgs/String.h>
-#include <stdio.h>
-#include "geometry_msgs/PoseStamped.h"
-#include "geometry_msgs/Vector3Stamped.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
+  mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+  current_state = *msg;
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "pub_setpoints");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "offb_node");
+  ros::NodeHandle nh;
 
-  ros::Publisher chatter_pub = n.advertise<geometry_msgs::PoseStamped>("/mavros/setpoint_position/local",100);
-  ros::Rate loop_rate(100);
-  ros::spinOnce();
+  ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+    ("mavros/state", 10, state_cb);
+  ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+    ("mavros/setpoint_position/local", 10);
+  ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+    ("mavros/cmd/arming");
+  ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+    ("mavros/set_mode");
 
-  geometry_msgs::PoseStamped msg;
-  int count = 1;
+  //the setpoint publishing rate MUST be faster than 2Hz
+  ros::Rate rate(20.0);
 
-  //PositionReciever qp;:
-  //Body some_object;
-  //qp.connect_to_server();
-
-
-  while(ros::ok()){
-    //some_object = qp.getStatus();
-    // some_object.print();
-    //printf("%f\n",some_object.position_x);
-    msg.header.stamp = ros::Time::now();
-    msg.header.seq=count;
-    msg.header.frame_id = "fcu";
-    msg.pose.position.x = 0.0;//0.001*some_object.position_x;
-    msg.pose.position.y = 0.0;//0.001*some_object.position_y;
-    msg.pose.position.z = 1.0;//0.001*some_object.position_z;
-    msg.pose.orientation.x = 0;
-    msg.pose.orientation.y = 0;
-    msg.pose.orientation.z = 0;
-    msg.pose.orientation.w = 1;
-
-    chatter_pub.publish(msg);
+  // wait for FCU connection
+  while(ros::ok() && current_state.connected){
     ros::spinOnce();
-    count++;
-    loop_rate.sleep();
+    rate.sleep();
   }
 
+  geometry_msgs::PoseStamped pose;
+  pose.pose.position.x = 0;
+  pose.pose.position.y = 0;
+  pose.pose.position.z = 2;
+
+  //send a few setpoints before starting
+  for(int i = 100; ros::ok() && i > 0; --i){
+    local_pos_pub.publish(pose);
+    ros::spinOnce();
+    rate.sleep();
+  }
+
+  mavros_msgs::SetMode offb_set_mode;
+  offb_set_mode.request.custom_mode = "OFFBOARD";
+
+  mavros_msgs::CommandBool arm_cmd;
+  arm_cmd.request.value = true;
+
+  ros::Time last_request = ros::Time::now();
+
+  while(ros::ok()){
+    if( current_state.mode != "OFFBOARD" &&
+	(ros::Time::now() - last_request > ros::Duration(5.0))){
+      if( set_mode_client.call(offb_set_mode) &&
+	  offb_set_mode.response.success){
+	ROS_INFO("Offboard enabled");
+      }
+      last_request = ros::Time::now();
+    } else {
+      if( !current_state.armed &&
+	  (ros::Time::now() - last_request > ros::Duration(5.0))){
+	if( arming_client.call(arm_cmd) &&
+	    arm_cmd.response.success){
+	  ROS_INFO("Vehicle armed");
+	}
+	last_request = ros::Time::now();
+      }
+    }
+
+    local_pos_pub.publish(pose);
+
+    ros::spinOnce();
+    rate.sleep();
+  }
 
   return 0;
 }
